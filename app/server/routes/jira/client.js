f177d9d481e8f09d8b4172652d3e1ec41bd195c1
Robbin Janssen <r.janssen@exonet.nl>
2018-05-18 20:42:09 +0200

    Get more jira data

--- app/server/routes/jira/client.js
+++ app/server/routes/jira/client.js
@@ -7,16 +7,6 @@ class Client {
   client;
 
   /**
-   * @type {number} Set the default maximum results that can be returned.
-   */
-  maxResults = 1000;
-
-  /**
-   * @type {number} The ID of the board to fetch the Sprints for.
-   */
-  boardId = 2;
-
-  /**
    * Create the Jira client based on the provided username and password.
    *
    * @param {string} host The Jira host name to connect to.
@@ -39,30 +29,99 @@ class Client {
    * @return {Promise} A promise to get the currently active Sprint.
    */
   getSprint({ boardId, sprintNumber }) {
-    return this.client.board.getSprintsForBoard({ boardId: this.boardId })
-      .then((response) => {
-        // Try to find the sprint.
-        const result = response.values.find(record => record.name === `Sprint ${sprintNumber}`);
+    console.log('Getting sprint');
+    return this.client.board.getSprintsForBoard({ boardId })
+      .then(
+        (response) => {
+          // Try to find the sprint.
+          const result = response.values.find(record => record.name === `Sprint ${sprintNumber}`);
 
-        // If the sprint is found, result, if not reject.
-        return typeof result !== 'undefined' ?
-          Promise.resolve(result) :
-          Promise.reject('Sprint not found');
-      });
+          console.log('Got sprint');
+
+          // If the sprint is found, result, if not reject.
+          return typeof result !== 'undefined' ?
+            Promise.resolve(result) :
+            Promise.reject(new Error('Sprint not found.'));
+        },
+        () => Promise.reject(new Error('Unable to get sprints.')),
+      );
   }
 
   /**
    * Get all the issues for a Sprint.
    *
-   * @param {number} sprintId The ID of the Sprint to get the issues for.
+   * @param {Object} sprint The sprint object containing the ID.
    *
    * @return {Promise} A promise to get all issues for a Sprint.
    */
-  getSprintIssues(sprintId) {
+  getIssuesBySprint(sprint) {
+    console.log('Getting sprint issues');
     return this.client.sprint.getSprintIssues({
-      sprintId,
-      maxResults: this.maxResults,
-    });
+      sprintId: sprint.id,
+      maxResults: 1000,
+    }).then(
+      (response) => {
+        console.log('Got issues');
+
+        return response.issues.length > 0 ?
+          Promise.resolve(response.issues.map((issue) => {
+            // console.log(issue.fields.epic);
+
+            return {
+              key: issue.key,
+              type: issue.fields.issuetype.name,
+              epic: typeof issue.fields.epic !== 'undefined' && issue.fields.epic !== null ?
+                issue.fields.epic.key :
+                null,
+              title: issue.fields.summary,
+              assignee: issue.fields.assignee !== null ?
+                issue.fields.assignee.name :
+                null,
+              completed: issue.fields.status.name === 'Done',
+              created: issue.fields.created,
+              worklogIds: issue.fields.worklog.worklogs.map(item => item.id),
+              timeTracking: {
+                estimate: issue.fields.timetracking.originalEstimateSeconds || 0,
+                spent: issue.fields.timetracking.timeSpentSeconds || 0,
+                remaining: issue.fields.timetracking.remainingEstimateSeconds || 0,
+              },
+            };
+          })) :
+          Promise.reject(new Error(`Unable to get issues for sprint with ID: ${sprint.id}`));
+      },
+      () => Promise.reject(new Error('Unable to get issues.'))
+    );
+  }
+
+  /**
+   * Get all the issues by given key.
+   *
+   * @param {string[]} keys The issue keys to get the issues by.
+   *
+   * @return {Promise} A promise to get all issues for the given keys.
+   */
+  getEpicsByKeys(keys) {
+    console.log('Getting epic issues');
+    return this.client.search.search({ jql: `key in (${keys.join(',')})` })
+      .then(
+        (response) => {
+          console.log('Got epics');
+
+          return response.issues.length > 0 ?
+            Promise.resolve(response.issues.map((issue) => {
+              // console.log(issue.fields.epic);
+
+              return {
+                key: issue.key,
+                name: issue.fields.customfield_10008,
+                title: issue.fields.summary,
+                completed: issue.fields.status.name === 'Done',
+              };
+            })) :
+            Promise.reject(new Error(`Unable to get issues for keys: ${keys.join(', ')}`));
+        },
+        () => Promise.reject(new Error('Unable to get issues.'))
+      );
   }
 }
 
--- app/server/routes/jira/index.js
+++ app/server/routes/jira/index.js
@@ -1,5 +1,69 @@
 import Router from 'express/lib/router';
+import moment from 'moment';
 import Client from './client';
+
+const prepare = ({ sprint, issues, epics }) => {
+  const totalIssues = issues.length;
+  const completedIssues = issues.filter(issue => issue.completed).length;
+
+  // Get users.
+  const users = [...new Set(issues
+    .filter(issue => issue.assignee !== null)
+    .map(issue => issue.assignee)
+    .sort(),
+  )].map((name) => {
+    const assigned = issues.filter(issue => issue.assignee === name);
+    const completed = assigned.filter(issue => issue.completed).length;
+
+    return {
+      name,
+      progress: {
+        total: assigned.length,
+        completed: completed,
+        percentage: Math.round((completed / assigned.length) * 100),
+      },
+      issues: assigned,
+    };
+  });
+
+  // Get epics.
+  const data = {
+    sprint: {
+      name: sprint.name,
+      goal: sprint.goal,
+      startDate: moment(sprint.startDate),
+      endDate: moment(sprint.endDate),
+      progress: {
+        total: totalIssues,
+        completed: completedIssues,
+        percentage: Math.round((completedIssues / totalIssues) * 100),
+      },
+    },
+    users,
+    epics: epics.map((epic) => {
+      const epicIssues = issues.filter(issue => issue.epic === epic.key);
+      const completed = epicIssues.filter(issue => issue.completed).length;
+
+      return {
+        ...epic,
+        completed: epicIssues.length === completed,
+        progress: {
+          total: epicIssues.length,
+          completed: completed,
+          percentage: Math.round((completed / epicIssues.length) * 100),
+        },
+        issues: epicIssues,
+      };
+    }),
+  };
+
+  console.dir(data);
+
+  return {
+    ...data,
+    issues,
+  };
+};
 
 /**
  * Get the routes for '/jira':
@@ -14,24 +78,40 @@ export default (config) => {
 
   // Setup the router.
   const router = new Router();
+  const data = {
+    sprint: {},
+    issues: [],
+    epics: [],
+  };
 
   router.get('/', (req, res) => {
     client.getSprint({ boardId: config.boardId, sprintNumber: config.sprintNumber })
       .then(
-        (sprint) => res.send(sprint),
-        (error) => res.send(error),
-      );
+        (sprint) => {
+          // Append the sprint to the data.
+          data.sprint = { ...sprint };
 
-    // client.getSprints()
-    //   .then(
-    //     result => res.send({
-    //       id: result.id,
-    //       attributes: {
-    //         ...result,
-    //       },
-    //     }),
-    //     error => console.log(error),
-    //   );
+          // Get issues for the current sprint.
+          return client.getIssuesBySprint(data.sprint);
+        },
+        error => res.send(error),
+      )
+      .then(
+        (issues) => {
+          data.issues = [...issues];
+
+          return client.getEpicsByKeys(data.issues.filter(issue => issue.epic !== null).map(issue => issue.epic));
+        },
+        error => res.send(error),
+      )
+      .then(
+        (epics) => {
+          data.epics = [...epics];
+
+          return res.send(prepare(data));
+        },
+        error => res.send(error),
+      );
   });
 
   return router;
